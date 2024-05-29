878a55004ce2f4297ab6cc8a56d5398604eb1de7
Maricielo9932 <maricieloramoslopez078@gmail.com>
2024-05-29 10:30:31 -0500

    tarea

---
+++ tipos_datos_estructurados/homework.py
@@ -0,0 +1,43 @@
+#crear una tisto de 5 alumnas atanacenaresos su nombre opettido y edad 
+lista_alumnos[{ 
+    "nombre":"abel",
+    "apellido": "rojas", 
+    "edad":27 
+    },{
+    "nombre":"cielo",
+    "apellido": "castro", 
+    "edad":23 
+    },{
+    "nombre": "ruth", 
+    "apellido":"castillo", 
+    "edad":18 
+    },{
+    "nombre":"flor", 
+    "apellido":"lucana", 
+    "edad":18 
+     },{
+    "nombre":"rocio", 
+    "apellido":"lobo", 
+    "edad":25 
+}]
+#insertor al final de la Lista los datos de antoni 
+lista_alumnos.append({ 
+    "nombre": "antoni",
+    "apellido":"cuevas", 
+    "edad":25 
+})
+print(lista_alumnos) 
+#eliminar de la Lista si existe Los datos de obel 
+lista_alumnos.remove({ 
+    "nombre": "abel", 
+    "apellido": "rojas", 
+    "edad":27 
+}) 
+print(lista_alumnos) 
+#atuano en la posicion a de Lo indice lista alumnos.
+index({ 
+"nombre": "rocio", 
+"apellido": "lobo", 
+"edad":25 
+}) 
+print(lista_alumnos[indice])
