33dc512d58b6aec05fd3ee72253ca476c4c48fa8
Maricielo9932 <maricieloramoslopez078@gmail.com>
2024-07-15 08:48:47 -0500

    tarea

---
+++ modulos/homework/main .py
@@ -0,0 +1,15 @@
+"""# ese es el script principal
+def es_mayor(edad):
+    """# funcion para saber si una persona es mayor de edad"""
+    if edad >= 18:
+        print("es mayor de edad")
+    else:
+        print("es menor de edad")
+
+def es_menor(lista)
+    """funcion para saber si el numero mayor de una lista"""
+    menor=lista[0]
+    for n in lista:
+        if n < menor:
+            menor=n
+            return mayor
